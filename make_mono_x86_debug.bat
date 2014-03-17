32dda8abedd236da45f1f082e874a0baba18ae1a
Mark Mennell <markmnl@outlook.com>
2014-03-17 15:22:25 +0800

    Replaced make mono bat scripts with msbuild file

--- make_mono_x86_debug.bat
+++
@@ -1,2 +0,0 @@
-IF NOT EXIST .\bin\Mono\x86\Debug\ MD .\bin\Mono\x86\Debug\
-dmcs -d:MONO;DEBUG -platform:x86 -unsafe -target:library -out:.\bin\Mono\x86\Debug\FalconUDP.dll *.cs
--- make_mono_x86_release.bat
+++
@@ -1,2 +0,0 @@
-IF NOT EXIST .\bin\Mono\x86\Release\ MD .\bin\Mono\x86\Release\
-dmcs -d:MONO;RELEASE -platform:x86 -unsafe -target:library -optimize -out:.\bin\Mono\x86\Release\FalconUDP.dll *.cs
