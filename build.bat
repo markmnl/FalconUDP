f70802b4dbf429032f4cc4c3f4751a654757dcf6
markmnl <markmnl@outlook.com>
2014-08-03 13:22:10 +0800

    Added build.bat

---
+++ build.bat
@@ -0,0 +1,6 @@
+msbuild FalconUDP.sln /p:Configuration=Debug
+msbuild FalconUDP.sln /p:Configuration=Release
+msbuild FalconUDPMono.msbuild /p:Configuration=Debug
+msbuild FalconUDPMono.msbuild /p:Configuration=Release
+msbuild FalconUDPMonoLinux.msbuild /p:Configuration=Debug
+msbuild FalconUDPMonoLinux.msbuild /p:Configuration=Release
