92b5a26ba3293669a44f2ca91c7e840f7cb22593
markmnl <markmnl@outlook.com>
2015-03-21 16:39:22 +0800

    Added MonoLinux configurations to sln, removed MonoLinux projects and updated build.bat

--- build.bat
+++ build.bat
@@ -1,8 +1,6 @@
 IF EXIST bin rmdir bin /S /Q
 IF EXIST obj rmdir obj /S /Q
 msbuild FalconUDP.sln /p:Configuration=Debug
+msbuild FalconUDP.sln /p:Configuration=MonoLinuxDebug
 msbuild FalconUDP.sln /p:Configuration=Release
-msbuild FalconUDPMono.msbuild /p:Configuration=Debug
-msbuild FalconUDPMono.msbuild /p:Configuration=Release
-msbuild FalconUDPMonoLinux.msbuild /p:Configuration=Debug
-msbuild FalconUDPMonoLinux.msbuild /p:Configuration=Release
+msbuild FalconUDP.sln /p:Configuration=MonoLinuxRelease
